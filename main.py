a93df8378ec567599abf1c3ea654367f00fc00b7
Obura Benjamin Onekalit <benonekalit@gmail.com>
2021-09-15 03:41:47 +0900

    jnpr.junos addition

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 import requests
 
 import sys
+from jnpr.junos import Devicefrom jnpr.junos.utils.config i mport Configfrom jnpr.junos.exception import *if __name__ == '__main            __':    try:        dev = Device(host='172.25.11.1', user='lab', pass       wd='lab123')        dev.open()        conf = Config(dev)        conf            .lock()        conf.load(path="bgp-config.conf", merge=True)        c       onf.commit()        conf.unlock()        print("Configuration applied      !")    except ConnectAuthError:
+    print("Authentication error!")              except ConnectTimeoutError:        print("NETCONF connection time   d out!")    except ConnectError as error:        print("Other connect        error: %s" % str(error))    except ConfigLoadError:        print("Fa      ilure when loading a configuration!")    except Exception as error:               print("Some other exception happened: %s" % str(error))    final ly:        dev.close()
 
 print(sys.executable)
 
