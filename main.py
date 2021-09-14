be493392da5c875a5650391cd43ff6b3538bb953
Obura Benjamin Onekalit <benonekalit@gmail.com>
2021-09-15 03:17:52 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#Write your code below this line 👇
+import os
+import requests
+
+import sys
+
+print(sys.version)
+print(sys.executable)
+
+def greet(who_to_greet):
+  greetings = 'Hello, {}'.format(who_to_greet)
+  return greetings
+
+
+print(greet('World'))
+print(greet('Benjamin'))
